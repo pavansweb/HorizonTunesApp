abf1f3b1545e26c0d051ab34b222d70a8d39fbd0
Pavan <pavansh555@gmail.com>
2024-02-23 20:42:43 +0530

    check

--- webPage/3.homePage.js
+++ webPage/3.homePage.js
@@ -504,21 +504,6 @@ volumeSlider.addEventListener("input", function() {
 
     //</loading>
 
- //cool cursor
-    const cursor = document.querySelector('.cursor');
-
-        document.addEventListener('mousemove', e => {
-            cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-        })
-
-        document.addEventListener('click', () => {
-            cursor.classList.add("expand");
-
-            setTimeout(() => {
-                cursor.classList.remove("expand");
-            }, 500)
-        })
-
         //<the end?>
 });
 
